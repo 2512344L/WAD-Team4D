0952b4fa0321ced369fc6d29981d4a7bcada1fd2
Oliver Billich <2469811b@student.gla.ac.uk>
2021-03-19 09:48:16 +0000

    Added Picture and Comment models in upskill_photography/models.py

--- upskill_photography/models.py
+++ upskill_photography/models.py
@@ -9,3 +9,28 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.username
+        
+
+class Picture(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(UserProfile)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='user_uploads/%d' % user.username)
+    thumbnail = models.ImageField(upload_to='thumbnails/%d' % user.username)
+    category = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=8, decimal_places=5)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return str(user.username) + " - " + str(self.title)
+        
+        
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    picture = models.ForeignKey(Picture)
+    user = models.ForeignKey(UserProfile)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
