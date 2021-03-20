a0a3fc1950394eae13d15dd0b037549518b44b0f
Rojhat802 <77485750+Rojhat802@users.noreply.github.com>
2021-03-20 16:40:45 +0000

    url mappings

--- upskill_photography/urls.py
+++ upskill_photography/urls.py
@@ -5,4 +5,14 @@ app_name = 'upskill_photography'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/', views.show_category, name='show_category'),
+    path('discovery/', views.discovery, name='discovery'),
+    path('Astronomy/', views.show_category, name='Astronomy'),
+    path('People/', views.show_category, name='People'),
+    path('Architecture/', views.show_category, name='Architecture'),
+    path('Uploads/', views.uploads, name="uploads"),
+    path('Account/', views.account, name="account"),
+    path('FAQ/', views.FAQ, name="FAQ"),
+    path('contact/', views.contact, name="contact"),
 ]
--- upskill_photography/views.py
+++ upskill_photography/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    
-    context_dict = {}
-    
-    return render(request, 'upskill_photography/index.html', context=context_dict)
+    return render(request, 'upskill_photography/index.html')
