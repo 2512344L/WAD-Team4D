739540bdd5d56cdadf037614a110f3ca51da61fd
Oliver Billich <2469811b@student.gla.ac.uk>
2021-03-20 17:32:22 +0000

    Added views in views.py according to urls.py

--- upskill_photography/views.py
+++ upskill_photography/views.py
@@ -1,4 +1,45 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'upskill_photography/index.html')
+    context_dict = {}
+    return render(request, 'upskill_photography/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/about.html', context=context_dict)
+
+def contact(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/contact.html', context=context_dict)
+
+def faq(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/faq.html', context=context_dict)
+
+def discovery(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/discovery.html', context=context_dict)
+
+def categories(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/categories.html', context=context_dict)
+
+def nature(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/category_nature.html', context=context_dict)
+
+def people(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/category_people.html', context=context_dict)
+
+def architecture(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/category_architecture.html', context=context_dict)
+
+def astronomy(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/category_astronomy.html', context=context_dict)
+
+def search_result(request):
+    context_dict = {}
+    return render(request, 'upskill_photography/search.html', context=context_dict)
