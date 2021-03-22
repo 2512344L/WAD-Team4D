fee34da3894f87cf821b5a960dc3d224e1833b7d
Oliver Billich <2469811b@student.gla.ac.uk>
2021-03-22 16:15:45 +0000

    Created single dynamic category page

--- upskill_photography/views.py
+++ upskill_photography/views.py
@@ -8,49 +8,48 @@ context_dict = {}
 context_dict['categories'] = Category.objects.all
 
 def index(request):
-    context_dict = {}
     # Retrieve the 10 most liked pictures and add them to the context dict
     context_dict['pictures'] = Picture.objects.order_by('-likes')[:10]
     return render(request, 'upskill_photography/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {}
     return render(request, 'upskill_photography/about.html', context=context_dict)
 
 def contact(request):
-    context_dict = {}
     return render(request, 'upskill_photography/contact.html', context=context_dict)
 
 def faq(request):
-    context_dict = {}
     return render(request, 'upskill_photography/faq.html', context=context_dict)
 
 def discovery(request):
-    context_dict = {}
     return render(request, 'upskill_photography/discovery.html', context=context_dict)
 
 def categories(request):
-    context_dict = {}
     return render(request, 'upskill_photography/categories.html', context=context_dict)
 
+def show_category(request, category_name):
+    try:
+        category = Category.objects.get(name=category_name)
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+    return render(request, 'upskill_photography/category.html', context=context_dict)
+
 def nature(request):
-    context_dict = {}
     return render(request, 'upskill_photography/category_nature.html', context=context_dict)
 
 def people(request):
-    context_dict = {}
     return render(request, 'upskill_photography/category_people.html', context=context_dict)
 
 def architecture(request):
-    context_dict = {}
     return render(request, 'upskill_photography/category_architecture.html', context=context_dict)
 
 def astronomy(request):
-    context_dict = {}
     return render(request, 'upskill_photography/category_astronomy.html', context=context_dict)
+
 def upload(request):
-    context_dict={}
     return render(request, 'upskill_photography/upload.html', context=context_dict)
+
 def search_result(request):
     if request.method == "POST":
         uploaded_file= request.FILES['document']
@@ -61,7 +60,6 @@ def search_result(request):
         query_text = (parse_qs(urlparse(request.build_absolute_uri()).query))['query'][0]
         keywords = query_text.split(' ')
         results = []
-        context_dict = {}
         if len(results) != 0:
             context_dict['query'] = f"Showing results for '{query_text}'"
         else:
@@ -70,10 +68,8 @@ def search_result(request):
 
 @login_required
 def account(request):
-    context_dict = {}
     return render(request, 'upskill_photography/account.html', context=context_dict)
 
 @login_required
 def uploads(request):
-    context_dict = {}
     return render(request, 'upskill_photography/uploads.html', context=context_dict)
