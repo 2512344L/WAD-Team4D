6a3fe302c07e282bbb92900c17379729c8e0dba8
Rojhat802 <77485750+Rojhat802@users.noreply.github.com>
2021-03-24 15:36:01 +0000

    fixed

--- upskill_photography/forms.py
+++ upskill_photography/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Picture
-class uploading(forms.modelForm):
+class uploading(forms.ModelForm):
     class Meta:
         model = Picture
         fields = ('uploading_user', 'title', 'image')
