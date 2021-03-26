fbb88e2bb8f3db7585bd4a9d574a5eeb53305ba7
Oliver Billich <2469811b@student.gla.ac.uk>
2021-03-26 17:33:25 +0000

    Even more work on the thumbnail tile

--- upskill_photography/templatetags/upskill_photography_template_tags.py
+++ upskill_photography/templatetags/upskill_photography_template_tags.py
@@ -5,6 +5,4 @@ register = template.Library()
 
 @register.inclusion_tag('upskill_photography/picture_thumbnail.html')
 def get_picture_thumbnail(picture=None):
-    width = picture.thumbnail.width
-    height = picture.thumbnail.height
-    return {'picture': picture, 'aspect_ratio': width/height}
+    return {'picture': picture}
