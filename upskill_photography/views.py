602817c3ebf4677994f0d83646bf9feaa0572e6f
Oliver Billich <2469811b@student.gla.ac.uk>
2021-03-26 16:37:45 +0000

    Added Page content for categories

--- upskill_photography/views.py
+++ upskill_photography/views.py
@@ -1,31 +1,28 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from upskill_photography.models import Picture, UserProfile, User, Category
 from urllib.parse import urlencode, urlparse, parse_qs
 
-from .models import Picture
 from django.core.files.storage import FileSystemStorage
-from upskill_photography.models import Picture, Category
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .forms import uploading
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.files.storage import FileSystemStorage
-from upskill_photography.models import Picture, Category
 from django.views.generic import ListView
 
 # Initialize the Context dict and add the categories to it by default
 context_dict = {}
-context_dict['categories'] = Category.objects.all
-
-# Initialize the Query String dict
-query_dict = {}
+context_dict['categories'] = list(Category.objects.all())
 
 # This should be called whenever we want to extract parameters from the request url string
 def get_query_parameters(request):
+    query_dict = {}
     if request.method == "GET":
         url_params = parse_qs(urlparse(request.build_absolute_uri()).query)
         for param in url_params:
             query_dict[param] = url_params[param][0]
+    return query_dict
             
 ## Private Method ##
 def picture_ordering(pictures, sort_style, sort_order):
@@ -90,7 +87,7 @@ def discovery(request):
         else:
             return redirect(reverse('upskill_photography:discovery'))
     else:
-        get_query_parameters(request)
+        query_dict = get_query_parameters(request)
         pictures = Picture.objects.order_by('-timestamp')
         
         if pictures and 'sort' in query_dict:
@@ -98,23 +95,58 @@ def discovery(request):
             pictures = picture_ordering(pictures, sort_style, sort_order)   
             context_dict['sort_style'] = sort_style
             context_dict['sort_order'] = sort_order
+        else:
+            context_dict['sort_style'] = "new"
+            context_dict['sort_order'] = "desc"
         
         context_dict['pictures'] = pictures
         return render(request, 'upskill_photography/discovery.html', context=context_dict)
 
 
 def categories(request):
+    pictures = []
+    for category in context_dict['categories']:
+        pictures = pictures + list(Picture.objects.filter(category=Category.objects.get(name=category.name)).order_by('-likes'))[0:1]
+    context_dict['pictures'] = pictures
     return render(request, 'upskill_photography/categories.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
-    # Pictures can have sort order
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        context_dict['category'] = None
-    return render(request, 'upskill_photography/category.html', context=context_dict)
+    if request.method == "POST":
+        query_string = {}
+        
+        sort_style = request.POST.get('sort_style', "")
+        sort_order = request.POST.get('sort_order', "")
+        if sort_style != "" and sort_order != "":
+            query_string['sort'] = sort_style + "_" + sort_order
+        
+        if len(query_string) > 0:
+            encoded_query_string = urlencode(query_string)
+            return redirect(reverse('upskill_photography:show_category', kwargs={'category_name_slug': category_name_slug}) + f"?{encoded_query_string}")
+        else:
+            return redirect(reverse('upskill_photography:show_category', kwargs={'category_name_slug': category_name_slug}))
+    else:
+        query_dict = get_query_parameters(request)
+        pictures = []
+        try:
+            category = Category.objects.get(slug=category_name_slug)
+            pictures = list(Picture.objects.filter(category=Category.objects.get(slug=category_name_slug)).order_by('-timestamp'))
+            context_dict['category'] = category
+        except Category.DoesNotExist:
+            context_dict['category'] = None
+            pictures = None
+        
+        if pictures and 'sort' in query_dict:
+            sort_style, sort_order = query_dict['sort'].split('_')
+            pictures = picture_ordering(pictures, sort_style, sort_order)   
+            context_dict['sort_style'] = sort_style
+            context_dict['sort_order'] = sort_order
+        else:
+            context_dict['sort_style'] = "new"
+            context_dict['sort_order'] = "desc"
+        
+        context_dict['pictures'] = pictures
+        return render(request, 'upskill_photography/category.html', context=context_dict)
 
 
 
@@ -137,7 +169,7 @@ def search_result(request):
         else:
             return redirect(reverse('upskill_photography:search_result'))
     else:
-        get_query_parameters(request)
+        query_dict = get_query_parameters(request)
         query_text = ""
         if 'query' in query_dict:
             query_text = query_dict['query']
@@ -183,6 +215,11 @@ def search_function(query_text):
 
 
 def userprofile(request, userprofile_username):
+    try:
+        user = User.objects.get(username=userprofile_username)
+        context_dict['user'] = UserProfile.objects.get(user=user)
+    except (User.DoesNotExist, UserProfile.DoesNotExist):
+        context_dict['user'] = None
     return render(request, 'upskill_photography/user_profile.html', context=context_dict)
 
 
