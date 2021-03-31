5a49a63e2a7eb50c7e4982727f2e270dedf7e836
Oliver Billich <2469811b@student.gla.ac.uk>
2021-03-31 12:09:21 +0100

    Small aesthetic changes to picture_view.html

--- static/js/upskill_photography-mapBox.js
+++ static/js/upskill_photography-mapBox.js
@@ -1,13 +1,20 @@
 var aspect_ratio;
+var map;
+var picture_location_marker;
 
 function toggleMapFunction() {
 	if( $('#map-container').is(':hidden') ) {
+		picture_location_marker = new mapboxgl.Marker()
+			.setLngLat([$('#like_btn').attr('data-longitude'), $('#like_btn').attr('data-latitude')])
+			.addTo(map);
 		$('#map-container').width($('#center-column').width());
 		$('#map-container').height($('#center-column').width() * aspect_ratio);
 		$("#map-container").css("display", "initial");
 		$("#img-container").css("display", "none");
 		$('#map-toggle').html("Show Picture");
+		map.zoom = 0;
 	} else {
+		picture_location_marker.remove();
 		$("#map-container").css("display", "none");
 		$("#img-container").css("display", "initial");
 		$('#map-toggle').html("Show Location");
@@ -15,7 +22,6 @@ function toggleMapFunction() {
 }
 
 $(document).ready(function(){
-	// Always switch back to picture view when resizing
 	aspect_ratio = $('#picture_image').height() / $('#picture_image').width();
 	$( window ).resize(function() {
 		if( $('#img-container').is(':hidden') ) {
@@ -30,14 +36,13 @@ $(document).ready(function(){
 	$('#comment_column').height($('#center-column').width() * aspect_ratio);
 	$('#map-container').append("<div id='map' style='width: " + $('#center-column').width() + "px; height: " + $('#center-column').width() * aspect_ratio + "px;'>");
 	$("#map-container").css("display", "none");
+	
 	mapboxgl.accessToken = 'pk.eyJ1IjoiMjQ2OTgxMWIiLCJhIjoiY2ttdWtuZ2RjMHZ5MzJ3bXc3b3J4N29ieSJ9.rR1dAX-utT26r3bIQRbcjA';
-	var map = new mapboxgl.Map({
+	map = new mapboxgl.Map({
 	container: 'map', // container ID
 	style: 'mapbox://styles/mapbox/streets-v11', // style URL
 	center: [$('#like_btn').attr('data-longitude'), $('#like_btn').attr('data-latitude')], // starting position [lng, lat]
 	zoom: 9 // starting zoom
 	});
-	var marker = new mapboxgl.Marker()
-		.setLngLat([$('#like_btn').attr('data-longitude'), $('#like_btn').attr('data-latitude')])
-			.addTo(map);
+	
 });
